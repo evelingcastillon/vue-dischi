7fe0d475af66c6fd34410bd2e32118fcb49b10e4
evelingcastillon <evelingcastillon0304@gmail.com>
2021-06-13 23:19:40 +0200

    correzione esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,20 @@ const app = new Vue ({
     el : '#app',
     data: {
         url: 'https://flynn.boolean.careers/exercises/api/array/music',
-        songs: []
+        songs: '',
+        errors: null
     },
     methods: {},
     mounted() {
         axios
         .get(this.url)
         .then(resp => {
-            this.songs.push(resp.data.response);
+            this.songs = resp.data.response;
             console.log(resp.data.response);
+        })
+        .catch( e => {
+            console.error(e);
+            this.errors = 'Sorry could no connect to the API' + e
         })
     }
 
